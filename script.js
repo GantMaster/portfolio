ec12835317d9733cb5363fd7c6216362e6780fe2
GantMaster <34738206+GantMaster@users.noreply.github.com>
2026-01-22 15:41:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // =========================
 
     const motionFiles = [
-        '5.mp4','8.mp4','7.mp4','3.png','4.png','1.png',
+        '5.mp4','8.mp4','7.mp4','3.jpg','4.jpg','1.jpg',
         '3.mp4','9.mp4','6.mp4','2.mp4','1.mp4','4.mp4',
         '10.mp4','12.mp4','13.mp4','11.mp4',
     ];
